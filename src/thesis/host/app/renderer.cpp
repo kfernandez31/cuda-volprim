21fe3bf93ca2f7601e8a12fd9753c1404f820d15
kfernandez31 <66901783+kfernandez31@users.noreply.github.com>
2025-07-30 12:22:29 +0200

    pushing

--- src/thesis/host/app/renderer.cpp
+++ src/thesis/host/app/renderer.cpp
@@ -84,7 +84,7 @@ void Renderer::initPrimsAndGAS()
         inst.instanceId        = i;
         inst.sbtOffset         = i;
         inst.visibilityMask    = 0xFF;
-        inst.flags             = OPTIX_RAY_FLAG_DISABLE_ANYHIT;
+        inst.flags             = OPTIX_RAY_FLAG_NONE;
         instances_[i]          = inst;
     }
 
